171c206c668ef8b5ed81b60e1d325adee7d00121
Aspsine <littleximail@gmail.com>
2015-09-09 17:46:14 +0800

    twitter style

---
+++ app/src/main/java/com/aspsine/swipetoloadlayout/demo/view/TwitterRefreshHeaderView.java
@@ -0,0 +1,114 @@
+package com.aspsine.swipetoloadlayout.demo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.aspsine.swipetoloadlayout.SwipeRefreshHeaderLayout;
+import com.aspsine.swipetoloadlayout.demo.R;
+
+/**
+ * Created by Aspsine on 2015/9/9.
+ */
+public class TwitterRefreshHeaderView extends SwipeRefreshHeaderLayout {
+
+    private ImageView ivArrow;
+
+    private TextView tvRefresh;
+
+    private ProgressBar progressBar;
+
+    private int mHeaderHeight;
+
+    private Animation rotateUp;
+
+    private Animation rotateDown;
+
+    private boolean rotated = false;
+
+    public TwitterRefreshHeaderView(Context context) {
+        this(context, null);
+    }
+
+    public TwitterRefreshHeaderView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TwitterRefreshHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mHeaderHeight = getResources().getDimensionPixelOffset(R.dimen.refresh_header_height_twitter);
+        rotateUp = AnimationUtils.loadAnimation(context, R.anim.rotate_up);
+        rotateDown = AnimationUtils.loadAnimation(context, R.anim.rotate_down);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        tvRefresh = (TextView) findViewById(R.id.tvRefresh);
+        ivArrow = (ImageView) findViewById(R.id.ivArrow);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+    }
+
+    @Override
+    public void onRefresh() {
+        Log.d("TwitterRefreshHeader", "onRefresh()");
+        ivArrow.clearAnimation();
+        ivArrow.setVisibility(GONE);
+        progressBar.setVisibility(VISIBLE);
+        tvRefresh.setText("REFRESHING");
+    }
+
+    @Override
+    public void onPrepare() {
+        Log.d("TwitterRefreshHeader", "onPrepare()");
+    }
+
+    @Override
+    public void onSwipe(int y) {
+        Log.d("TwitterRefreshHeader", "onSwipe(" + y + ")");
+        ivArrow.setVisibility(VISIBLE);
+        progressBar.setVisibility(GONE);
+        if (y > mHeaderHeight) {
+            tvRefresh.setText("RELEASE TO REFRESH");
+            if (!rotated) {
+                ivArrow.clearAnimation();
+                ivArrow.startAnimation(rotateUp);
+                rotated = true;
+            }
+        } else if (y < mHeaderHeight) {
+            if (rotated) {
+                ivArrow.clearAnimation();
+                ivArrow.startAnimation(rotateDown);
+                rotated = false;
+            }
+
+            tvRefresh.setText("SWIPE TO REFRESH");
+        }
+    }
+
+    @Override
+    public void complete() {
+        Log.d("TwitterRefreshHeader", "complete()");
+        rotated = false;
+        ivArrow.clearAnimation();
+        ivArrow.setVisibility(GONE);
+        progressBar.setVisibility(GONE);
+        tvRefresh.setText("COMPLETE");
+    }
+
+    @Override
+    public void onReset() {
+        Log.d("TwitterRefreshHeader", "onReset()");
+        rotated = false;
+        ivArrow.clearAnimation();
+        ivArrow.setVisibility(GONE);
+        progressBar.setVisibility(GONE);
+    }
+
+}
