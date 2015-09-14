3a7691fb2b4dd18ecaa7d758ece17a7243d54f9f
Aspsine <littleximail@gmail.com>
2015-09-14 18:36:01 +0800

    progressbar

--- app/src/main/java/com/aspsine/swipetoloadlayout/demo/view/header/GoogleRefreshHeaderView.java
+++ app/src/main/java/com/aspsine/swipetoloadlayout/demo/view/header/GoogleRefreshHeaderView.java
@@ -3,24 +3,38 @@ package com.aspsine.swipetoloadlayout.demo.view.header;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import com.aspsine.swipetoloadlayout.SwipeRefreshTrigger;
 import com.aspsine.swipetoloadlayout.SwipeTrigger;
+import com.aspsine.swipetoloadlayout.demo.R;
 
 /**
  * Created by aspsine on 15/9/10.
  */
 public class GoogleRefreshHeaderView extends FrameLayout implements SwipeTrigger, SwipeRefreshTrigger {
+    private ProgressBar progressBar;
+
+    private int mHeaderHeight;
+
     public GoogleRefreshHeaderView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public GoogleRefreshHeaderView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public GoogleRefreshHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mHeaderHeight = context.getResources().getDimensionPixelOffset(R.dimen.refresh_header_height_google);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        progressBar.setMax(mHeaderHeight);
     }
 
     @Override
@@ -35,7 +49,7 @@ public class GoogleRefreshHeaderView extends FrameLayout implements SwipeTrigger
 
     @Override
     public void onSwipe(int y) {
-
+        progressBar.setProgress(y);
     }
 
     @Override
