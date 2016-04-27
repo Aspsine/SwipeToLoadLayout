9a37c1d65ea374783c61cdd4f165d3f88c663576
Aspsine <littleximail@gmail.com>
2016-04-28 01:44:54 +0800

    Demo use FragmentNavigator

--- app/src/main/java/com/aspsine/swipetoloadlayout/demo/MainActivity.java
+++ app/src/main/java/com/aspsine/swipetoloadlayout/demo/MainActivity.java
@@ -3,46 +3,67 @@ package com.aspsine.swipetoloadlayout.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import com.aspsine.swipetoloadlayout.demo.fragment.BaseNavigationFragment;
+import com.aspsine.fragmentnavigator.FragmentNavigator;
 import com.aspsine.swipetoloadlayout.demo.fragment.BaseToolbarFragment;
-import com.aspsine.swipetoloadlayout.demo.fragment.NavGoogleFragment;
-import com.aspsine.swipetoloadlayout.demo.fragment.NavJDFragment;
-import com.aspsine.swipetoloadlayout.demo.fragment.NavJavaCodeFragment;
-import com.aspsine.swipetoloadlayout.demo.fragment.NavTwitterFragment;
-import com.aspsine.swipetoloadlayout.demo.fragment.NavYalantisFragment;
 
-public class MainActivity extends AppCompatActivity implements BaseToolbarFragment.ToggleDrawerCallBack
-        , NavigationView.OnNavigationItemSelectedListener {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+        , BaseToolbarFragment.ToggleDrawerCallBack {
+
+    private static final Integer ID_ARRAY[] = {
+            R.id.nav_Twitter_style,
+            R.id.nav_google_style,
+            R.id.nav_yalantis_style,
+            R.id.nav_jd_style,
+            R.id.nav_set_header_footer_via_code
+    };
+
+    private static final List<Integer> IDS = Arrays.asList(ID_ARRAY);
+
+    private static final int DEFAULT_POSITION = 0;
+
     private DrawerLayout drawerLayout;
-    private Handler handler;
+
+    /**
+     * https://github.com/Aspsine/FragmentNavigator
+     */
+    private FragmentNavigator mFragmentNavigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mFragmentNavigator = new FragmentNavigator(getSupportFragmentManager(), new MainFragmentAdapter(), R.id.container);
+
+        mFragmentNavigator.setDefaultPosition(DEFAULT_POSITION);
+
+        mFragmentNavigator.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
 
         navigationView.setNavigationItemSelectedListener(this);
 
-        handler = new Handler(Looper.getMainLooper());
+        navigationView.setCheckedItem(IDS.get(DEFAULT_POSITION));
 
-        if (savedInstanceState == null) {
-            navigationView.setCheckedItem(R.id.nav_Twitter_style);
-            showNavigationFragment(R.id.nav_Twitter_style);
-        }
+        mFragmentNavigator.showFragment(mFragmentNavigator.getCurrentPosition());
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mFragmentNavigator.onSaveInstanceState(outState);
     }
 
     @Override
@@ -50,70 +71,20 @@ public class MainActivity extends AppCompatActivity implements BaseToolbarFragme
         drawerLayout.openDrawer(GravityCompat.START);
     }
 
-    int lastNavItemId = -1;
-
     @Override
     public boolean onNavigationItemSelected(final MenuItem menuItem) {
         drawerLayout.closeDrawer(GravityCompat.START);
-        handler.postDelayed(new Runnable() {
+        drawerLayout.postDelayed(new Runnable() {
             @Override
             public void run() {
                 int itemId = menuItem.getItemId();
                 if (itemId == R.id.nav_about) {
                     startActivity(new Intent(MainActivity.this, AboutActivity.class));
                 } else {
-                    showNavigationFragment(itemId);
+                    mFragmentNavigator.showFragment(IDS.indexOf(itemId));
                 }
             }
         }, 200);
         return true;
-    }
-
-    void showNavigationFragment(int itemId) {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        Fragment lastFragment = fm.findFragmentByTag(getTag(lastNavItemId));
-        lastNavItemId = itemId;
-        if (lastFragment != null) {
-            ft.detach(lastFragment);
-        }
-        Fragment fragment = fm.findFragmentByTag(getTag(itemId));
-        if (fragment == null) {
-            fragment = getItem(itemId);
-            ft.add(R.id.container, fragment, getTag(itemId));
-        } else {
-            ft.attach(fragment);
-        }
-        ft.commit();
-    }
-
-    private BaseNavigationFragment getItem(int itemId) {
-        BaseNavigationFragment navigationFragment = new BaseNavigationFragment();
-
-        switch (itemId) {
-            case R.id.nav_Twitter_style:
-                navigationFragment = NavTwitterFragment.newInstance();
-                break;
-
-            case R.id.nav_google_style:
-                navigationFragment = NavGoogleFragment.newInstance();
-                break;
-
-            case R.id.nav_yalantis_style:
-                navigationFragment = NavYalantisFragment.newInstance();
-                break;
-
-            case R.id.nav_jd_style:
-                navigationFragment = NavJDFragment.newInstance();
-                break;
-            case R.id.nav_set_header_footer_via_code:
-                navigationFragment = NavJavaCodeFragment.newInstance();
-                break;
-        }
-        return navigationFragment;
-    }
-
-    private String getTag(int itemId) {
-        return String.valueOf(itemId);
     }
 }
